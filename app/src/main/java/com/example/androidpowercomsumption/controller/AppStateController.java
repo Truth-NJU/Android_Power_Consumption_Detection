e22b957501cc95d857e1407fcff51300566d31d2
Truth360 <tzh19850355091@163.com>
2022-08-17 19:58:47 +0800

    complete feature:appstate

--- app/src/main/java/com/example/androidpowercomsumption/controller/AppStateController.java
+++ app/src/main/java/com/example/androidpowercomsumption/controller/AppStateController.java
@@ -1,6 +1,10 @@
 package com.example.androidpowercomsumption.controller;
 
+import android.util.Log;
+
 public class AppStateController {
+
+    private static final String TAG = "AppStateApplication";
     public long startTime; // 监控开始时间
 
     public long endTime; // 监控结束时间
@@ -9,10 +13,29 @@ public class AppStateController {
 
     public long backgroundTime; // 后台运行时长
 
-    public long chargingTime; // 充电时长
+    public boolean status; // true 前台 false 后台
+    public long curStatusStartTime; // 当前状态的开始时间
 
-    public long screenTime; // 息屏时长
+    public long curStatusEndTime; // 当前状态的结束时间
 
-    public void start(){
+    public double foregroundRatio; //前台运行时间占比
+
+    public double backgroundRatio; // 后台运行时间占比
+
+    public void start() {
+        this.startTime = System.currentTimeMillis();
+    }
+
+    public void finish() {
+        this.endTime = System.currentTimeMillis();
+        this.foregroundRatio = foregroundTime * 1.0 / (this.endTime - this.startTime);
+        this.backgroundRatio = backgroundTime * 1.0 / (this.endTime - this.startTime);
+        Log.d(TAG, "前台运行时间:" + this.foregroundTime);
+        Log.d(TAG, "后台运行时间:" + this.backgroundTime);
+        Log.d(TAG, "总运行时间:" + (this.endTime - this.startTime));
+        Log.d(TAG, String.valueOf(this.foregroundRatio));
+        Log.d(TAG, String.valueOf(this.backgroundRatio));
+
+
     }
 }
--- app/src/main/java/com/example/androidpowercomsumption/utils/AppStateApplication.java
+++ app/src/main/java/com/example/androidpowercomsumption/utils/AppStateApplication.java
@@ -5,10 +5,12 @@ import android.app.Activity;
 import android.app.Application;
 import android.os.Bundle;
 import android.util.Log;
+import com.example.androidpowercomsumption.controller.AppStateController;
 
 
 public class AppStateApplication extends Application {
     private static final String TAG = "AppStateApplication";
+
     /**
      * onCreate是一个回调接口，android系统会在应用程序启动的时候，在任何应用程序组件（activity、服务、
      * 广播接收器和内容提供者）被创建之前调用这个接口。
@@ -24,17 +26,30 @@ public class AppStateApplication extends Application {
 
     //声明一个监听Activity们生命周期的接口
     static class MyActivityLifecycleCallbacks implements ActivityLifecycleCallbacks {
+
+        private final AppStateController appStateController = new AppStateController();
+
         /**
          * application下的每个Activity声明周期改变时，都会触发以下的函数。
          */
         @Override
         public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
-
+            // 只会在启动的时候触发一次
+            appStateController.start();
+            appStateController.status = true; // 前台状态
+            appStateController.curStatusStartTime = appStateController.startTime; // 当前状态的开始时间
         }
 
         @Override
         public void onActivityStarted(Activity activity) {
             Log.d(TAG, "APP进入前台");
+            if (!appStateController.status) { // 后台进入前台
+                appStateController.status = true;
+                appStateController.curStatusEndTime = System.currentTimeMillis();// 后台状态的结束时间
+                appStateController.backgroundTime += (appStateController.curStatusEndTime - appStateController.curStatusStartTime);
+                appStateController.curStatusStartTime = System.currentTimeMillis();// 后台进入前台，前台状态的开始时间
+
+            }
         }
 
         @Override
@@ -48,6 +63,14 @@ public class AppStateApplication extends Application {
         @Override
         public void onActivityStopped(Activity activity) {
             Log.d(TAG, "App进入后台");
+            if (appStateController.status) { // 由前台进入后台
+                appStateController.status = false;
+                appStateController.curStatusEndTime = System.currentTimeMillis(); // 前台状态的结束时间
+                appStateController.foregroundTime += (appStateController.curStatusEndTime - appStateController.curStatusStartTime);
+                appStateController.curStatusStartTime = System.currentTimeMillis(); // 前台进入后台，后台状态的开始时间
+            }
+
+
         }
 
         @Override
@@ -56,7 +79,10 @@ public class AppStateApplication extends Application {
 
         @Override
         public void onActivityDestroyed(Activity activity) {
+            appStateController.finish();
         }
-    };
+    }
+
+    ;
 
 }
